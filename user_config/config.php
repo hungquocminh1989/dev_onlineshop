80ba783bb9be9155251b4ebec19c793d57293e31
Minh <hungquocminh1989@gmail.com>
2017-08-20 01:59:28 +0700

    Revert "update config"
    This reverts commit a95b871c976d64550da123d49aa0f4a5f41e0a49.
    
    # Conflicts:
    #	.htaccess

--- user_config/config.php
+++ user_config/config.php
@@ -1,6 +1,6 @@
 <?php
-define('ACW_PROJECT', 'shop');
-define('ACW_BASE_URL', 'http://127.0.0.1/shop/');	
+define('ACW_PROJECT', 'mintywatch.shop');
+define('ACW_BASE_URL', 'http://mintywatch.shop/');	
 define('AKAGANE_SALT', 'akg1$%');
-define('ACW_SESSION_PARENT_DIR', 'C://acwork_session/');
+define('ACW_SESSION_PARENT_DIR', '/home/u952687329/public_html/tmp/acwork_session/');
 define('ACW_SESSION_DIR', ACW_SESSION_PARENT_DIR . '/');
