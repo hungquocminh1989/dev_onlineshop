3f0dfdcaa6b6301f258e7cc3a4f9d5dc367607b7
Minh <hungquocminh1989@gmail.com>
2017-08-20 18:14:56 +0700

    Update UI

--- user_config/define.php
+++ user_config/define.php
@@ -1,38 +1,37 @@
 <?php
-define('ACW_FOOTER_COPY_RIGHT', '2017 - Admin');
-/*
- * CMSタイトル
- */
-define('AKAGANE_TITLE', 'Pimlus'); //Edit LIXD-13 hungtn VNIT 20150803
+define('ACW_FOOTER_COPY_RIGHT', '2017 - Bản Quyền Thuộc Về '.ACW_PROJECT);
 
 /*
  * シリーズ情報
  */
 define('AKAGANE_STRAGE_PATH', ACW_ROOT_DIR . '/data/');
 
-// エクスポート(インポート)処理の添付ファイル名のエンコード
-define('AKAGANE_EXPORT_FILE_ENCODING_XML',    'UTF-8');    // XML 内に記載している文字コード
-define('AKAGANE_EXPORT_FILE_ENCODING_SYSTEM', 'SJIS-win'); // ファイルシステム(OS)の文字コード(実ファイル名で保存の際に使用)
-
 //Phone
 define('HTML_PHONE', '0902676026');
+
+//Địa Chỉ
+define('HTML_ADDRESS', '364 Bùi Hữu Nghĩa Phường 2 Quận Bình Thạnh TP.HCM');
+
+//Email
+define('HTML_EMAIL', 'ABC@gmail.com');
 
 //Curency
 define('HTML_CURRENCY', 'đ');
 
 //Deliver Message
-define('HTML_DELIVER_TITLE', 'abc');
-define('HTML_DELIVER_INFO_TITLE_1', 'Miễn Phí Giao Hàng');
+define('HTML_DELIVER_TITLE', '');
+define('HTML_DELIVER_INFO_TITLE_1', 'MIỄN PHÍ GIAO HÀNG');
 define('HTML_DELIVER_INFO_CONTENT_1', 'Trong vòng 3 ngày.');
-define('HTML_DELIVER_INFO_TITLE_2', 'Quà Tặng Hấp Dẫn');
+define('HTML_DELIVER_INFO_TITLE_2', 'QUÀ TẶNG HẤP DẪN');
 define('HTML_DELIVER_INFO_CONTENT_2', 'Nhiều quà tặng ưu đãi.');
-define('HTML_DELIVER_INFO_TITLE_3', 'ABC');
-define('HTML_DELIVER_INFO_CONTENT_3', 'ABC');
-define('HTML_DELIVER_INFO_TITLE_4', 'XYZ');
-define('HTML_DELIVER_INFO_CONTENT_4', 'XYZ');
+define('HTML_DELIVER_INFO_TITLE_3', 'GIAO HÀNG NHẬN TIỀN');
+define('HTML_DELIVER_INFO_CONTENT_3', 'Thanh toán đơn hàng bằng hình thức trực tiếp');
+define('HTML_DELIVER_INFO_TITLE_4', 'ĐẶT HÀNG ONLINE');
+define('HTML_DELIVER_INFO_CONTENT_4', HTML_PHONE);
 
 //Product Trending
 define('HTML_PRODUCT_TRENDING_TITLE', 'Xu Hướng');
+
 
 
 // ------------------------------------
