9f25cb899db19a813f2dd805e0d99bdaef6bcf28
Minh <hungquocminh1989@gmail.com>
2017-08-28 22:28:44 +0700

    Update code

--- app/model/common/category.php
+++ app/model/common/category.php
@@ -19,6 +19,50 @@ class category_common_model extends ACWModel
 		
 		return $this->query($sql,$sql_param);
 	}
+	
+	public function _insertCategory($ctg_name)
+	{
+		$sql = "
+			INSERT INTO m_category (ctg_name, add_datetime, upd_datetime, del_flg)
+			VALUES (:ctg_name, NOW(), NOW(), 0);
+		";
+		$sql_param = array();
+		$sql_param['ctg_name'] = $ctg_name;
+		
+		return $this->execute($sql,$sql_param);
+	}
+	
+	public function _updateCategory($m_ctg_id, $ctg_name)
+	{
+		$sql = "
+			UPDATE m_category
+			SET ctg_name = :ctg_name,
+				add_datetime = NOW()
+			WHERE m_ctg_id = :m_ctg_id;
+		";
+		$sql_param = array();
+		$sql_param['ctg_name'] = $ctg_name;
+		$sql_param['m_ctg_id'] = $m_ctg_id;
+		
+		return $this->execute($sql,$sql_param);
+	}
+	
+	public function _deleteCategory($m_ctg_id, $ctg_name)
+	{
+		$sql_ctg = "
+			DELETE FROM m_category
+			WHERE m_ctg_id = :m_ctg_id;
+		";
+		$sql_product = "
+			DELETE FROM m_product
+			WHERE m_ctg_id = :m_ctg_id;
+		";
+		$sql_param = array();
+		$sql_param['m_ctg_id'] = $m_ctg_id;
+		
+		$this->execute($sql_ctg,$sql_param);
+		$this->execute($sql_product,$sql_param);
+	}
 
 }
 /* ファイルの終わり */
--- app/model/common/product.php
+++ app/model/common/product.php
@@ -30,6 +30,48 @@ class product_common_model extends ACWModel
 		
 		return $this->query($sql,$sql_param);
 	}
+	
+	public function _insertProduct($ctg_name)
+	{
+		$sql = "
+			INSERT INTO m_product (m_ctg_id, product_name, product_price, product_info, discount_price, group_type, add_datetime, upd_datetime, del_flg)
+			VALUES (:m_ctg_id, :product_name, :product_price, :product_info, :discount_price, :group_type, NOW(), NOW(), 0);
+		";
+		$sql_param = array();
+		$sql_param['m_ctg_id'] = $m_ctg_id;
+		$sql_param['product_name'] = $product_name;
+		$sql_param['product_price'] = $product_price;
+		$sql_param['product_info'] = $product_info;
+		$sql_param['discount_price'] = $discount_price;
+		$sql_param['group_type'] = $group_type;
+		
+		return $this->execute($sql,$sql_param);
+	}
+	
+	public function _updateProduct($ctg_name)
+	{
+		$sql = "
+			UPDATE m_product
+			SET m_ctg_id = :m_ctg_id, 
+				product_name = :product_name, 
+				product_price = :product_price, 
+				product_info = :product_info, 
+				discount_price = :discount_price, 
+				group_type = :group_type, 
+				upd_datetime = NOW(), 
+				del_flg = 0
+			WHERE m_product_id = :m_product_id
+		";
+		$sql_param = array();
+		$sql_param['m_ctg_id'] = $m_ctg_id;
+		$sql_param['product_name'] = $product_name;
+		$sql_param['product_price'] = $product_price;
+		$sql_param['product_info'] = $product_info;
+		$sql_param['discount_price'] = $discount_price;
+		$sql_param['group_type'] = $group_type;
+		
+		return $this->execute($sql,$sql_param);
+	}
 
 }
 /* ファイルの終わり */
