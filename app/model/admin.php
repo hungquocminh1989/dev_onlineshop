d54b59e9a14a70af401e3792418cfdd5f1b10488
Minh <hungquocminh1989@gmail.com>
2017-09-11 00:35:27 +0700

    code screen menu

--- app/model/admin.php
+++ app/model/admin.php
@@ -10,7 +10,7 @@ class admin_model extends ACWModel
 		$oProduct = new product_common_model();
 		$oCategory = new category_common_model();
 		
-		$result_menu_header = $oMenu->_getMenuHeader();
+		$result_menu_header = $oMenu->_getMenu();
 		$product_main = $oProduct->_getProduct();
 		$result_category = $oCategory->_getCategory();
 		
@@ -29,30 +29,39 @@ class admin_model extends ACWModel
 		
 		$param = self::get_param(array(
                     'acw_url'
+                    , 'm_menu_id'
                     , 'menu_name'
+                    , 'menu_mobile'
                     , 'menu_link'
                     , 'menu_type'
                     , 'sort_no'
         ));
 		
 		$oMenu = new menu_common_model();
-		$oProduct = new product_common_model();
-		$oCategory = new category_common_model();
 		
-		$oMenu->_insertMenu($param);
+		if($param['m_menu_id']!=""){
+			$oMenu->_updateMenu($param);
+		}
+		else{
+			$oMenu->_insertMenu($param);
+		}
 		
-		$result_menu_header = $oMenu->_getMenuHeader();
-		$product_main = $oProduct->_getProduct();
-		$result_category = $oCategory->_getCategory();
 		
-		$template_data = array();
-		$template_data["header_main"] = $result_menu_header;
-		$template_data["slider_main"] = "";
-		$template_data["category_main"] = $result_category;
-		$template_data["product_main"] = $product_main;
-		$template_data["other_main"] = "";
-		$template_data["footer_main"] = "";
-		return ACWView::template('admin.html',$template_data);
+		return ACWView::json(NULL);
+	}
+	
+	public static function action_delmenu()
+	{
+		
+		$param = self::get_param(array(
+                    'acw_url'
+                    , 'm_menu_id'
+        ));
+		
+		$oMenu = new menu_common_model();
+		
+		$oMenu->_deleteMenu($param);
+		return ACWView::json(NULL);
 	}
 	
 }
